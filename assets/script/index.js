e9aff1634284983122e06144ae315e2a117b8e62
Luan Carvalho <luancarvprofissional@gmail.com>
2022-12-06 03:04:19 -0300

    First page

---
+++ assets/script/index.js
@@ -0,0 +1,24 @@
+//NAV
+const btnExpMenu = document.getElementById("btnExpMenu")
+btnExpMenu.addEventListener("click", navMobileMenu)
+const expMenu = document.getElementById("expMenu")
+expMenu.style.height = '0px'
+
+function navMobileMenu() {
+    if (expMenu.style.height == '0px') {
+        expMenu.style.height = '280px';
+        expMenu.style.borderBottom = "solid 1px rgba(0, 0, 0, 0.25)";
+    }
+    else { expMenu.style.height = '0px'; expMenu.style.borderBottom = "none" }
+
+}
+
+setTimeout(() => {
+    const x = document.querySelector("#eapps-google-reviews-3d5503b7-ac55-4620-a445-0f173eadb11b > div > a")
+    console.log(x)
+    x.style.display = "none"
+}, 1000);
+
+
+//Test 
+
