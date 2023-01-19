f12a350d31055f7b6739057a1dd988fea406071e
Luan Carvalho <luancarvprofissional@gmail.com>
2023-01-19 19:43:22 -0300

    V1

--- assets/script/index.js
+++ assets/script/index.js
@@ -12,27 +12,25 @@ function navMobileMenu() {
     else { expMenu.style.height = '0px'; expMenu.style.borderBottom = "none" }
 
 }
+const v1 = "#eapps-google-reviews-dcdb998f-8458-4878-8409-cf13321ecbaa > div > a"
+const v2 = "#eapps-google-reviews-3d5503b7-ac55-4620-a445-0f173eadb11b > div > a"
 
 setTimeout(() => {
-    const x = document.querySelector("#eapps-google-reviews-3d5503b7-ac55-4620-a445-0f173eadb11b > div > a")
-    console.log(x)
-    x.style.display = "none"
-
-    const y =document.querySelector("#eapps-instagram-feed-1 > a")
-    console.log(y)
-    y.style.display = "none"
+    RemWater()
 }, 5000);
 
 setTimeout(() => {
-    const x = document.querySelector("#eapps-google-reviews-3d5503b7-ac55-4620-a445-0f173eadb11b > div > a")
+    RemWater()
+}, 10000);
+
+function RemWater(){
+    const x = document.querySelector(v1)
     console.log(x)
     x.style.display = "none"
 
     const y =document.querySelector("#eapps-instagram-feed-1 > a")
     console.log(y)
     y.style.display = "none"
-}, 10000);
+}
 
-
-//Test 
-
+//Test
